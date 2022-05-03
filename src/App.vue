357684a7d2a4c23df3883687d06e2e6d2bde006f
Ale <alegonzalez1993@gmail.com>
2022-05-02 23:26:33 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Desafío</h1>
+    <Avatar
+      v-for="(personaje, $index) of personajes"
+      :key="$index"
+      :nombre="personaje.nombre"
+      :imagen="personaje.src"
+    /> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Avatar from "@/components/Avatar.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: { 
+    Avatar 
+  },
+  data: () => ({
+    personajes: [
+      {
+        nombre: "Rick Sanchez",
+        src: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      },
+      {
+        nombre: "Morty Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+      },
+      {
+        nombre: "Summer Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+      },
+      {
+        nombre: "Beth Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+      },
+      {
+        nombre: "Jerry Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+      },
+    ]
+  })
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+body {
+  font-family: 'Creepster', cursive;
+  background-image: url("../src/assets/background.png");
+  background-size: cover;
+  padding-bottom: 9rem;
 }
 </style>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="avatar">
+    <img :src="imagen" alt="">
+    <p>{{ nombre }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["nombre", "imagen"]
+}
+</script>
+
+<style>
+
+.avatar {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin: 1rem;
+}
+
+img {
+  border-radius: 50%;
+  height: 72px;
+  width: 72px;
+}
+
+p {
+  color: rgb(222, 233, 222);
+  margin: 0;
+  font-size: 2rem;
+}
+</style>
